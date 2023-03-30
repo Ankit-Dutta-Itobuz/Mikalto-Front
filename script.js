dd6bc38c6172a2d113caadf4b164e1b4b1e5cf0f
dutta_ankit <ankit.dutta@itobuz.com>
2023-03-30 11:20:38 +0530

    new repo

---
+++ script.js
@@ -0,0 +1,60 @@
+let res;
+
+let swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+async function getData() {
+  res = await fetch(`http://localhost:8000`)
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      alert(err);
+    });
+  if (!res.error) {
+    console.log(res);
+  } else {
+    console(res.error.message);
+  }
+  document.getElementById(
+    "landingBg"
+  ).style.backgroundImage = `url(${res.landingSec.image})`;
+  document.getElementById(
+    "landingSubText"
+  ).textContent = `${res.landingSec.landingText}`;
+  document.getElementById("priceDataStandard").textContent =`${res.carouselSec.carousel1.text}`;
+  document.getElementById("imgStandard").style.backgroundImage = `url(${res.carouselSec.carousel1.image})`;
+  document.getElementById("priceDataConference").textContent =`${res.carouselSec.carousel2.text}`;
+  document.getElementById("imgConference").style.backgroundImage = `url(${res.carouselSec.carousel2.image})`;
+  document.getElementById("priceDataPremium").textContent =`${res.carouselSec.carousel3.text}`;
+  document.getElementById("imgPremium").style.backgroundImage = `url(${res.carouselSec.carousel3.image})`;
+}
+getData();
+function formHandle(event) {
+  event.preventDefault();
+  let formData = {
+    checkIn: document.getElementById("checkIn").value,
+    checkOut: document.getElementById("checkOut").value,
+    adults: document.getElementById("adult").value,
+    childrens: document.getElementById("children").value,
+  };
+  console.log(formData);
+  fetch("http://localhost:8000/", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(formData),
+  }).then(() => {
+    alert("Form submitted!");
+    document.getElementById("checkIn").value = null;
+    document.getElementById("checkOut").value = null;
+    document.getElementById("adult").value = "0";
+    document.getElementById("children").value = "0";
+  });
+}
+
